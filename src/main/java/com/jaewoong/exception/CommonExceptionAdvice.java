47c60214cf08e11b8a6cb2557d1a1dd69aa70762
jason967 <jason967@naver.com>
2020-06-02 02:29:27 +0900

    chapter2 끝
    error 404 해결 못함 --

--- src/main/java/com/jaewoong/exception/CommonExceptionAdvice.java
+++ src/main/java/com/jaewoong/exception/CommonExceptionAdvice.java
@@ -1,10 +1,13 @@
 package com.jaewoong.exception;
 
 import lombok.extern.log4j.Log4j;
-import org.springframework.http.HttpStatus;
+
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -12,18 +15,19 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 //해당 객체가 스프링의 컨트롤러에서 발생하는 예외를 처리하는 존재임을 명시
 @Log4j
 public class CommonExceptionAdvice {
+
     @ExceptionHandler(Exception.class)
     public String exception(Exception ex, Model model)
     {
         log.error("Exception......"+ex.getMessage());
         model.addAttribute("exception",ex);
-        return "error_page";
+        return "error/error_page";
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handler404(NoHandlerFoundException ex)
-    {
-        return "custom404";
+    public String handle404(NoHandlerFoundException ex) {
+        log.error("error404");
+        return "error/custom404";
     }
 }
--- src/main/webapp/WEB-INF/views/error/error_page.jsp
+++ src/main/webapp/WEB-INF/views/error/error_page.jsp
@@ -6,6 +6,8 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false" import="java.util.*" %>
 <html>
 <head>
     <title>Error_page</title>
