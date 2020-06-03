b706a57a29429947a4f1dfc71305cc91eff1f6e4
jason967 <jason967@naver.com>
2020-06-03 13:50:03 +0900

    CRUDcomplete

--- src/main/java/com/jaewoong/mapper/BoardMapper.java
+++ src/main/java/com/jaewoong/mapper/BoardMapper.java
@@ -7,6 +7,16 @@ import java.util.List;
 
 public interface BoardMapper {
     public List<BoardVO> getList();
+
+    public void insert(BoardVO board);
+
+    public void insertSelectKey(BoardVO board);
+
+    public BoardVO read(Long bno);
+
+    public int delete(Long bno);
+
+    public int update(BoardVO board);
     //@Select("select * from tbl_board where bno >0")
 
 }
