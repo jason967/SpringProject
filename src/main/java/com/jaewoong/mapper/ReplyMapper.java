5715c6508a9c4e65e96ad0b0695a85b9af0c3029
jason967 <jason967@naver.com>
2020-06-12 17:06:35 +0900

    go

--- src/main/java/com/jaewoong/mapper/ReplyMapper.java
+++ src/main/java/com/jaewoong/mapper/ReplyMapper.java
@@ -1,4 +1,11 @@
 package com.jaewoong.mapper;
 
+import com.jaewoong.domain.ReplyVO;
+
 public interface ReplyMapper {
+    public int insert(ReplyVO vo);
+
+    public ReplyVO read(Long bno);
+    //특정 댓글의 삭제는 댓글의 번호(rno)만 처리가 가능하다.
+    public int delete(Long bno);
 }
