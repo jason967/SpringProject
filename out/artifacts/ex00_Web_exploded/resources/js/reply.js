476f3a0940a5635df048ea20a82a61b0cd2492c2
jason967 <jason967@naver.com>
2020-06-22 15:35:37 +0900

    SetupDesk

--- out/artifacts/ex00_Web_exploded/resources/js/reply.js
+++ out/artifacts/ex00_Web_exploded/resources/js/reply.js
@@ -4,27 +4,46 @@ var replyService=(function(){
     function add(reply,callback) {
         console.log("reply........");
 
-
-    $.ajax({
-        type:'post',
-        url:'/replies/new',
-        data:JSON.stringify(reply),
-        contentType:"application/json; charset=utf-8",
-        success : function(result,status,xhr)
-        {
-            if(callback)
+        $.ajax({
+            type:'post',
+            url:'/replies/new',
+            data:JSON.stringify(reply),
+            contentType:"application/json; charset=utf-8",
+            success : function(result,status,xhr)
             {
-                callback(result);
-            }
-        },
-        error:function (xhr,status,er) {
-            if(error)
-            {
-                error(er);
-            }
+                if(callback)
+                {
+                    callback(result);
+                }
+            },
+            error:function (xhr,status,er) {
+                if(error)
+                {
+                    error(er);
+                }
 
-        }
+            }
         })
     }
-    return {add:add};
+    function getList(param,callback,error) {
+        var bno = param.bno;
+        var page = param.page||1;
+
+        $.getJSON("/replies/pages/"+bno+'/'+page+".json",
+            function (data) {
+                if(callback)
+                {
+                    callback(data);
+                }
+            }).fail(function (xhr,status,err) {
+            if(error)
+            {
+                error();
+            }
+        });
+    }
+    return {
+        add:add,
+        getList:getList
+    };
 })();
--- src/main/webapp/resources/js/reply.js
+++ src/main/webapp/resources/js/reply.js
@@ -3,46 +3,47 @@ var replyService=(function(){
 
     function add(reply,callback) {
         console.log("reply........");
-    $.ajax({
-        type:'post',
-        url:'/replies/new',
-        data:JSON.stringify(reply),
-        contentType:"application/json; charset=utf-8",
-        success : function(result,status,xhr)
-        {
-            if(callback)
+
+        $.ajax({
+            type:'post',
+            url:'/replies/new',
+            data:JSON.stringify(reply),
+            contentType:"application/json; charset=utf-8",
+            success : function(result,status,xhr)
             {
-                callback(result);
+                if(callback)
+                {
+                    callback(result);
+                }
+            },
+            error:function (xhr,status,er) {
+                if(error)
+                {
+                    error(er);
+                }
+
             }
-        },
-        error:function (xhr,status,er) {
-            if(error)
-            {
-                error(er);
-            }
-        }
         })
     }
-
     function getList(param,callback,error) {
-
         var bno = param.bno;
-        var page = param.page || 1;
+        var page = param.page||1;
 
-        $.getJSON("/replies/pages/"+bno+"/"+page +".json",
+        $.getJSON("/replies/pages/"+bno+'/'+page+".json",
             function (data) {
                 if(callback)
                 {
                     callback(data);
                 }
-            }).fail(function (xhmr,status,err) {
-                if(error) {
-                    error();
-                }
-        })
+            }).fail(function (xhr,status,err) {
+            if(error)
+            {
+                error();
+            }
+        });
     }
     return {
         add:add,
-        getList :getList
+        getList:getList
     };
 })();
